93cb68e15057a8362ec7c18e668ffcf42105ea63
Ohanyan Gohar <goharohanyana@gmail.com>
2020-07-30 21:56:06 +0400

    Updates

---
+++ precache-manifest.fefce824137eab58a2440e05b8439806.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b0e92db1d8c95db1a4337039bb38aef",
+    "url": "/react-todo/index.html"
+  },
+  {
+    "revision": "959b30ab7b158ec6b701",
+    "url": "/react-todo/static/css/main.6d9fd715.chunk.css"
+  },
+  {
+    "revision": "7e700270e6b27eb3a829",
+    "url": "/react-todo/static/js/2.ff7d02d3.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/react-todo/static/js/2.ff7d02d3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "959b30ab7b158ec6b701",
+    "url": "/react-todo/static/js/main.febb27e5.chunk.js"
+  },
+  {
+    "revision": "2e4dd0157cf095f8730b",
+    "url": "/react-todo/static/js/runtime-main.b50245dd.js"
+  }
+]);
